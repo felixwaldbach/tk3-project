5bd8fc97be4b923b3d7de74db80681ad8f7afd16
Patrick Schmidt <ps.patti1998@gmail.com>
2021-06-16 13:11:00 +0200

    * Fixed requesting of permission for API

--- app/src/main/java/de/darmstadt/tk/MainActivity.kt
+++ app/src/main/java/de/darmstadt/tk/MainActivity.kt
@@ -1,36 +1,24 @@
 package de.darmstadt.tk
 
 import android.Manifest
-import android.app.PendingIntent
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.Settings
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.viewModelScope
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
-import com.google.android.gms.common.util.PlatformVersion
-import com.google.android.gms.location.*
-import com.google.android.material.snackbar.Snackbar
-import de.darmstadt.tk.background.ActivityReceiver
-import de.darmstadt.tk.background.SleepReceiver
 import de.darmstadt.tk.background.SleepWorker
 import de.darmstadt.tk.ui.theme.SensingAppTheme
 import de.patrick.keyattestation.MainViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 class MainActivity : ComponentActivity() {
@@ -38,28 +26,54 @@ class MainActivity : ComponentActivity() {
     val TAG = "MainActivity"
     private val viewModel by viewModels<MainViewModel>()
 
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                Log.i(TAG, "Perm was GRANTED")
+            } else {
+                Log.i(TAG, "Perm not granted")
+            }
+        }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
-
 //        setupWorkers()
-        setupActivityTransition()
-        setupSleepTransition()
+        checkPermission()
 
-        Log.d(TAG,"Is permission granted: ${checkSelfPermission(Manifest.permission.ACTIVITY_RECOGNITION)}")
         setContent {
             SensingAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Button(onClick = {
-                    }) {
-                        Text(text = "Click me")
-
-                    }
+                    MainScreen(viewModel::startTracking)
                 }
             }
         }
+    }
+
+    private fun checkPermission() {
+        viewModel.viewModelScope.launch(Dispatchers.Default) {
+            when {
+                ContextCompat.checkSelfPermission(
+                    baseContext,
+                    Manifest.permission.ACTIVITY_RECOGNITION
+                ) == PackageManager.PERMISSION_GRANTED -> {
+                    Log.i(TAG,"Permission ACTIVITY_RECOGNITION GRANTED")
+                }
+                else -> {
+                    // You can directly ask for the permission.
+                    // The registered ActivityResultCallback gets the result of this request.
+                    requestPermissionLauncher.launch(
+                        Manifest.permission.ACTIVITY_RECOGNITION
+                    )
+                }
+            }
+        }
+
     }
 
     private fun setupWorkers() {
@@ -69,59 +83,6 @@ class MainActivity : ComponentActivity() {
         WorkManager.getInstance(applicationContext).enqueue(sleepWorker)
     }
 
-    private fun setupActivityTransition() {
-        Log.d(TAG, "setupActivityTransition")
-        val transitions = mutableListOf<ActivityTransition>()
-
-        transitions += ActivityTransition.Builder()
-            .setActivityType(DetectedActivity.STILL)
-            .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
-            .build()
-        transitions += ActivityTransition.Builder()
-            .setActivityType(DetectedActivity.STILL)
-            .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
-            .build()
-
-        val request = ActivityTransitionRequest(transitions)
-
-        val pendingIntent = PendingIntent.getBroadcast(
-            applicationContext,
-            0,
-            Intent(applicationContext, ActivityReceiver::class.java),
-            PendingIntent.FLAG_CANCEL_CURRENT
-        )
-
-        ActivityRecognition.getClient(applicationContext)
-            .requestActivityTransitionUpdates(request, pendingIntent)
-    }
-
-    private fun setupSleepTransition() {
-        Log.d(TAG, "setupSleepTransition")
-        val pendingIntent = PendingIntent.getBroadcast(
-            applicationContext,
-            0,
-            Intent(applicationContext, SleepReceiver::class.java),
-            PendingIntent.FLAG_CANCEL_CURRENT
-        )
-
-        ActivityRecognition.getClient(applicationContext).requestSleepSegmentUpdates(
-            pendingIntent,
-            SleepSegmentRequest.getDefaultSleepSegmentRequest()
-        )
-    }
 
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SensingAppTheme {
-        Greeting("Android")
-    }
-}
--- app/src/main/java/de/darmstadt/tk/MainViewModel.kt
+++ app/src/main/java/de/darmstadt/tk/MainViewModel.kt
@@ -1,12 +1,16 @@
 package de.patrick.keyattestation
 
+import android.annotation.SuppressLint
 import android.app.Application
+import android.app.PendingIntent
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.Intent
 import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Base64
+import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
@@ -14,6 +18,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.android.gms.location.*
+import de.darmstadt.tk.background.ActivityReceiver
+import de.darmstadt.tk.background.SleepReceiver
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.security.KeyPairGenerator
@@ -21,6 +28,14 @@ import java.security.KeyStore
 import java.security.cert.Certificate
 import java.security.cert.X509Certificate
 import java.security.spec.PKCS8EncodedKeySpec
+import androidx.annotation.NonNull
+
+import com.google.android.gms.tasks.OnFailureListener
+
+import com.google.android.gms.tasks.OnSuccessListener
+
+
+
 
 
 class MainViewModel(var appCtx: Application) : AndroidViewModel(appCtx) {
@@ -28,12 +43,65 @@ class MainViewModel(var appCtx: Application) : AndroidViewModel(appCtx) {
 
     var state by mutableStateOf(0);
 
-
+    fun startTracking() {
+        setupActivityTransition()
+        setupSleepTransition()
+    }
 
     fun startAttestation() {
         viewModelScope.launch(Dispatchers.Default) {
 
         }
+    }
+
+    private fun setupActivityTransition() {
+        Log.d(TAG, "setupActivityTransition")
+        val transitions = mutableListOf<ActivityTransition>()
+
+        transitions += ActivityTransition.Builder()
+            .setActivityType(DetectedActivity.STILL)
+            .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_ENTER)
+            .build()
+        transitions += ActivityTransition.Builder()
+            .setActivityType(DetectedActivity.STILL)
+            .setActivityTransition(ActivityTransition.ACTIVITY_TRANSITION_EXIT)
+            .build()
+
+        val request = ActivityTransitionRequest(transitions)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+            appCtx,
+            0,
+            Intent(appCtx, ActivityReceiver::class.java),
+            PendingIntent.FLAG_CANCEL_CURRENT
+        )
+
+        val task = ActivityRecognition.getClient(appCtx)
+            .requestActivityTransitionUpdates(request, pendingIntent)
+
+        task.addOnSuccessListener(
+            OnSuccessListener<Void?> {
+                Log.i(TAG,"Transitions Api was successfully registered.")
+            })
+        task.addOnFailureListener(
+            OnFailureListener { e ->
+                Log.e(TAG, "Transitions Api could NOT be registered: $e")
+            })
+    }
+
+    private fun setupSleepTransition() {
+        Log.d(TAG, "setupSleepTransition")
+        val pendingIntent = PendingIntent.getBroadcast(
+            appCtx,
+            0,
+            Intent(appCtx, SleepReceiver::class.java),
+            PendingIntent.FLAG_CANCEL_CURRENT
+        )
+
+        ActivityRecognition.getClient(appCtx).requestSleepSegmentUpdates(
+            pendingIntent,
+            SleepSegmentRequest.getDefaultSleepSegmentRequest()
+        )
     }
 
 }
